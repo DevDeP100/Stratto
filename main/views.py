f05c0ae217023904d0d473e82e634ad817ca1478
Dep Sistemas <desenvolvimento@depsistemas.com.br>
2025-05-27 23:12:26 +0100

    v4.6

--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Contato
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        mensagem = request.POST.get('mensagem')
+        
+        # Criar novo contato
+        Contato.objects.create(
+            nome=nome,
+            email=email,
+            mensagem=mensagem
+        )
+        
+        messages.success(request, 'Mensagem enviada com sucesso! Entraremos em contato em breve.')
+        return redirect('home')
+    
     return render(request, 'main/home.html')
